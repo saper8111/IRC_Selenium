797eca313bde7edef3500a075b4c89d68fe91c80
Vyacheslav <saper.test81@gmail.com>
2020-02-08 21:44:53 +0300

    Однократный запуск браузера. Версия 1. Создан класс TestSuiteFixture.

---
+++ Irc_web_test/TestSuiteFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebIrcTests
+{
+    [SetUpFixture]
+
+    public class TestSuiteFixture
+    {
+        [SetUp]
+
+        public void InitApplicationManager()
+        {
+
+        }
+
+        [TearDown]
+
+        public void StopApplicationManager()
+        {
+
+        }
+    }
+}
